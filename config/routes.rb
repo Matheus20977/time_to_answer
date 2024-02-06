9e44b0762a8365296e2da293a8997acd081d0578
Matheus20977 <matheus20977@gmail.com>
2024-02-06 14:48:34 -0300

    questions model CRUD setup (front & backend)

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     get 'welcome/index' # Dashboard
     resources :admins # Administradores
     resources :subjects # Assuntos
+    resources :questions # Perguntas
   end
   
   devise_for :users
