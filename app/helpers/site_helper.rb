b7bb0041dfc3269709aa67febf5bf96ab13b592d
Matheus <matheus20977@gmail.com>
2024-08-13 12:44:14 -0300

    answer check changes and add subject filtering

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,13 @@
 module SiteHelper
+  def msg_jumbotron
+    case params[:action]
+      when 'index'
+        "Últimas perguntas cadastradas..."
+      when 'questions'
+        "Resultados para o termo \"#{params[:term]}\"..."
+      when 'subject'
+        subject = Subject.find(params[:subject_id]).description
+        "Resultados para o assunto \"#{subject}\"..."
+    end
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -8,12 +8,18 @@ class Question < ApplicationRecord
 
   # Scopes
   scope :search, -> (page, term) {
-    includes(:answers)
+    includes(:answers, :subject)
     .where("lower(description) LIKE ?", "%#{term.downcase}%")
     .page(page)
   }
 
   scope :last_questions, -> (page) {
-    includes(:answers).order('id desc').page(page)
+    includes(:answers, :subject).order('id desc').page(page)
+  }
+
+  scope :search_subject, -> (page, subject_id) {
+    includes(:answers, :subject)
+    .where(subject_id: subject_id)
+    .page(page)
   }
 end
